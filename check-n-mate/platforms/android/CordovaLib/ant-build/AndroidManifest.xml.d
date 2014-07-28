ca1e7552983d98afc444d36fd313d1c44bd30e29
Chinmay <chinmaygupta28@gmail.com>
2014-07-29 00:07:51 +0530

    ionic added

---
+++ check-n-mate/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/chinmay/check-n-mate/check-n-mate/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/chinmay/check-n-mate/check-n-mate/platforms/android/CordovaLib/AndroidManifest.xml \
