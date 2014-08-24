e85099db2a644dffdfa07b3e043132c93637eecc
Chinmay <chinmaygupta28@gmail.com>
2014-08-24 14:26:45 +0530

    new structure

--- check-n-mate/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/chinmay/check-n-mate/check-n-mate/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/chinmay/check-n-mate/check-n-mate/platforms/android/CordovaLib/AndroidManifest.xml \
