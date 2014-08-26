dd3b0722972bc5d50dc9fb131a5238fb9f0fe755
Chinmay <chinmaygupta28@gmail.com>
2014-08-27 00:53:10 +0530

    ad

---
+++ dev/frontend/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/AndroidManifest.xml : \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/AndroidManifest.xml \
---
+++ dev/frontend/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,13 @@
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes.dex : \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/org/apache/cordova/device/Device.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/R$attr.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/R$xml.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/R$drawable.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/checknmate.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/R$string.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/R.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionicframework/checknmate850490/BuildConfig.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$2.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/classes/com/ionic/keyboard/IonicKeyboard$1.class \
+/home/chinmay/check-n-mate/dev/frontend/platforms/android/ant-build/dexedLibs/classes-fba658ee12c1a86c32f09de200e10fe7.jar \
